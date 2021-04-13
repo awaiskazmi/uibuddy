490d4b4e7fbf38aa7761e4a574cf2bf43f0c26d4
Awais Kazmi <devs.awais@gmail.com>
2021-04-14 01:01:18 +0500

    File paths improved and css duplicates removed

--- nocss.js
+++ nocss.js
@@ -20,63 +20,64 @@ const glob = require("glob");
 const concat = require("concat");
 const postcss = require("postcss");
 
+const pathtoWatch = __dirname + "/";
+
 // 1. watch folder for changes
-chokidar.watch(__dirname + "/*.html").on("all", (event, filePath) => {
-  desinationFilename = filePath.split("/");
-  desinationFilename = desinationFilename[desinationFilename.length - 1].split(
-    "."
-  )[0];
-  console.log("\x1b[36m%s\x1b[0m", "File change detected...");
+chokidar
+  .watch("*.html", { persistent: true, cwd: pathtoWatch })
+  .on("all", (event, filePath) => {
+    desinationFilename = filePath.split(".")[0];
+    console.log("\x1b[36m%s\x1b[0m", "File change detected...");
 
-  // 2 & 3. get changed file path and read file
-  fs.readFile(filePath, "utf-8", function(err, data) {
-    if (err) throw err;
+    // 2 & 3. get changed file path and read file
+    fs.readFile(filePath, "utf-8", function (err, data) {
+      if (err) throw err;
 
-    finalOutput = magic.Magic(
-      parser.cssClasses(data),
-      parser.componentClasses(data)
-    );
-
-    // 4. generate css for that file in a temp folder
-    // fs.writeFile(filename, output, function(err) {
-    mkdirp(__dirname + "/tmp").then(made => {
-      var written = fs.writeFile(
-        __dirname + "/tmp/" + desinationFilename + ".css",
-        finalOutput,
-        "utf8",
-        function(err, data) {
-          if (err) throw err;
-          glob(__dirname + "/tmp/*.css", {}, function(err, files) {
-            // 5. concat all generated css files
-            concat(files).then(css => {
-              console.log("\x1b[33m%s\x1b[0m", "...concatenating files...");
-              // 6. run postcss on (5)
-              postcss([
-                require("postcss-combine-duplicated-selectors"),
-                require("autoprefixer"),
-                require("css-mqpacker")
-              ])
-                .process(css, {
-                  from: "style.css",
-                  to: "style.min.css"
-                })
-                .then(result => {
-                  console.log("\x1b[33m%s\x1b[0m", "...applying postcss...");
-                  // 7. generate single style.min.css file
-                  finalCss = fs.writeFile(
-                    __dirname + "/style.min.css",
-                    result.css,
-                    "utf8",
-                    function(err, data) {
-                      if (err) throw err;
-                      console.log("\x1b[32m%s\x1b[0m", "NOCSS complete!");
-                    }
-                  );
-                });
-            });
-          });
-        }
+      finalOutput = magic.Magic(
+        parser.cssClasses(data),
+        parser.componentClasses(data)
       );
+
+      // 4. generate css for that file in a temp folder
+      mkdirp(__dirname + "/tmp").then((made) => {
+        var written = fs.writeFile(
+          __dirname + "/tmp/" + desinationFilename + ".css",
+          finalOutput,
+          "utf8",
+          function (err, data) {
+            if (err) throw err;
+            glob(__dirname + "/tmp/*.css", {}, function (err, files) {
+              // 5. concat all generated css files
+              concat(files).then((css) => {
+                console.log("\x1b[33m%s\x1b[0m", "...concatenating files...");
+                // 6. run postcss on (5)
+                postcss([
+                  require("postcss-combine-duplicated-selectors"),
+                  require("postcss-discard-duplicates"),
+                  require("autoprefixer"),
+                  require("css-mqpacker"),
+                ])
+                  .process(css, {
+                    from: "style.css",
+                    to: "style.min.css",
+                  })
+                  .then((result) => {
+                    console.log("\x1b[33m%s\x1b[0m", "...applying postcss...");
+                    // 7. generate single style.min.css file
+                    finalCss = fs.writeFile(
+                      __dirname + "/style.min.css",
+                      result.css,
+                      "utf8",
+                      function (err, data) {
+                        if (err) throw err;
+                        console.log("\x1b[32m%s\x1b[0m", "NOCSS complete!");
+                      }
+                    );
+                  });
+              });
+            });
+          }
+        );
+      });
     });
   });
-});
