f784f5b5b9de6690d6417c92fdfbb21bb90a75f8
Awais Kazmi <devs.awais@gmail.com>
2021-04-13 18:07:23 +0500

    Page Title Updated

--- addition.js
+++
@@ -1,4 +0,0 @@
-var exports = (module.exports = {});
-exports.AddNumber = function(a, b) {
-  return a + b;
-};
--- app.js
+++ app.js
@@ -1,40 +1,11 @@
-console.log("...nocss...");
-
-function css(el) {
-  var sheets = document.styleSheets,
-    ret = [];
-  classList = el.classList.value;
-
-  // el.matches =
-  //   el.matches ||
-  //   el.webkitMatchesSelector ||
-  //   el.mozMatchesSelector ||
-  //   el.msMatchesSelector ||
-  //   el.oMatchesSelector;
-  // for (var i in sheets) {
-  //   var rules = sheets[i].rules || sheets[i].cssRules;
-  //   for (var r in rules) {
-  //     if (el.matches(rules[r].selectorText)) {
-  //       // console.log(rules[r].style.cssText);
-  //       // ret.push(rules[r].cssText);
-  //       // ret.push(rules[r].style.cssText.replace(/\s/g, ''));
-  //       toReplace = " !important";
-  //       ret.push(rules[r].style.cssText.replace(" !important", ""));
-  //     }
-  //   }
-  // }
-  // return ret.join("");
-  return classList.split(" ");
-}
-
-// 1. Select all nodes referencing other
+// 1. Select all nodes referencing other components
 nodes = document.querySelectorAll("[ref]");
 
 if (nodes.length > 0) {
   // 2. Loop through all referencing nodes
   nodes.forEach(function(node) {
     nodeReference = node.getAttribute("ref");
-    // 4. Set styles on node
+    // 3. Apply component class on each node
     node.classList.add("component-" + nodeReference);
   });
 } else {
