882375f224cd930409f64e1f30aa5b888ad12797
Awais Kazmi <devs.awais@gmail.com>
2021-04-27 09:32:37 +0500

    Package implementation updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 const { watch, src, dest, task } = require("gulp");
 const nocss = require("./index.js");
+const concat = require("gulp-concat");
+const postcss = require("gulp-postcss");
+const combine = require("postcss-combine-duplicated-selectors");
+const duplicate = require("postcss-discard-duplicates");
+const autoprefixer = require("autoprefixer");
+const mqpacker = require("css-mqpacker");
 
-const build = function(cb) {
+const build = function() {
+  const plugins = [combine(), duplicate(), autoprefixer(), mqpacker()];
   return src("./*.html")
     .pipe(nocss())
-    .pipe(dest("dist/"));
-  cb();
+    .pipe(concat("style.min.css"))
+    .pipe(postcss(plugins))
+    .pipe(dest("./"));
 };
 build.displayName = "nocss";
 
--- index.js
+++ index.js
@@ -7,28 +7,45 @@ const parser = require("./parseHTML.js"),
   through = require("through2"),
   gutil = require("gulp-util"),
   pluginError = gutil.PluginError,
+  replaceExtension = require("replace-ext"),
   postcss = require("postcss"),
-  fs = require("fs");
+  path = require("path");
 
-var nocss = function() {
-  // read file
+const nocss = function() {
+  // read single file
   return through.obj(function(file, enc, callback) {
+    // ERROR HANDLING
+    if (file.isNull()) {
+      return cb(null, file);
+    }
+    if (file.isStream()) {
+      return cb(new PluginError(PLUGIN_NAME, "Streaming not supported"));
+    }
+    if (path.basename(file.path).indexOf("_") === 0) {
+      return cb();
+    }
+    if (!file.contents.length) {
+      file.path = replaceExtension(file.path, ".css"); // eslint-disable-line no-param-reassign
+      return cb(null, file);
+    }
+
+    // IF NO ERRORS
     var isBuffer = false,
       string = "",
       css = "";
 
-    // file to buffer
+    // convert single file to buffer
     isBuffer = file.isBuffer();
 
     if (isBuffer) {
-      // convert file buffer to string
+      // convert single file buffer to string
       string = new String(file.contents);
       // generate css from string
       css += magic.Magic(
         parser.cssClasses(string),
         parser.componentClasses(string)
       );
-      // run postcss
+      // run postcss on single file
       postcss([
         require("postcss-combine-duplicated-selectors"),
         require("postcss-discard-duplicates"),
@@ -40,19 +57,16 @@ var nocss = function() {
           to: "style.min.css"
         })
         .then(result => {
-          // generate style.min.css
-          fs.writeFile(
-            __dirname + "/style.min.css",
-            result.css,
-            "utf8",
-            function(err, data) {
-              if (err) throw err;
-              console.log("\x1b[32m%s\x1b[0m", "nocss build complete!");
-            }
-          );
+          // generate single .css file
+          var x = result.css;
+          var outBuffer = Buffer.from(x);
+          var aFile = new gutil.File();
+          aFile.contents = outBuffer;
+          aFile.path = replaceExtension(file.path, ".css");
+
+          callback(null, aFile);
         });
     }
-    callback();
   });
 };
 
